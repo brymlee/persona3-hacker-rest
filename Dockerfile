4f0a65eb142a7a491670271f4760298f1a36e052
Brymlee <brymlee1911@gmail.com>
2021-08-10 12:17:14 -0600

    Displaying some stuff in rest

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
 FROM archlinux:latest
-RUN pacman -Syu --noconfirm nodejs npm curl stack
+RUN pacman -Syu --noconfirm nodejs npm curl stack make gcc
+RUN stack update
+RUN stack setup
 RUN mkdir rest
 WORKDIR rest
+COPY persona3-hacker/persona3-hacker.cabal persona3-hacker.cabal 
+RUN stack init
+COPY persona3-hacker/src src
+RUN stack build
 COPY package.json package.json
 RUN npm i
 COPY index.js index.js
--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const doResponse = (_request) => (response) => {
+  const characters = [];
+  const result = btoa(JSON.stringify({ characters }));
+  const h = `stack run -- "${result}"`;
+  require('child_process').exec(h, (error, stdout, stderr) => {
+    if(stderr || error || stdout === undefined || stdout === null){
+      response.status(404);
+    } else {
+      response.setHeader('content-type', 'text/plain');
+      response.send(`${new Date().getMilliseconds()} ${stdout}`);
+    }
+  });
+};
+
 require('express')()
   .use(require('body-parser').json())
-  .get('/', (_request, response) => {
-    response.send('hello');
+  .get('/', (request, response) => {
+    doResponse(request)(response);
   }).post('/', (request, response) => {
-    const { body } = request;
-    response.send(`${body}`);
+    doResponse(request)(response);
   }).listen(80, () => {
     console.log('App listening on port 80');
   });
